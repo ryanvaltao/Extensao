06be5b0b628b7eb5d31e95b67bde2f42c8e85725
ryanvaltao <rv_moura@id.uff.br>
2026-05-09 14:14:51 -0300

    Script - Tarefas 1 a 3 - Etapa 2

--- extensao-esqueleto.R
+++ extensao-esqueleto.R
@@ -482,15 +482,22 @@ write.csv(SINASC_GO, "SINASC_GO.csv", row.names = FALSE)
 # verificar se a leitura foi feita corretamente e a estrutura dos dados
 # nomeie o banco de dados como dados_sim
 
+dados_sim = read.csv("Mortalidade_Geral_2015.csv", header = TRUE, sep = ';')
+
 # Tarefa 2. Reduzir dados_sim apenas para as colunas que serão utilizadas, nomeando este novo banco de dados como dados_sim_1
 # as colunas serão: 1, 3, 4, 8, 9, 10, 11, 14, 17, 35, 36, 37, 47, 77, 84
 # nomes das respectivas variáveis: CONTADOR, TIPOBITO, DTOBITO, DTNASC, IDADE, SEXO, RACACOR, ESC2010, CODMUNRES, TPMORTEOCO, 
 # OBITOGRAV, OBITOPUERP, CAUSABAS, TPOBITOCOR, MORTEPARTO
 
+dados_sim_1 = dados_sim[, c(1, 3, 4, 8, 9, 10, 11, 14, 17, 35, 36, 37, 47, 77, 84)]
+names(dados_sim_1)
+
 # Tarefa 3. Reduzir dados_sim_1 apenas para o estado que o aluno irá trabalhar (utilizar os dois primeiros dígitos de CODMUNRES), nomeando este novo banco de dados como dados_sim_2
 # Códigos das UF: 11: RO, 12: AC, 13: AM, 14: RR, 15: PA, 16: AP, 17: TO, 21: MA, 22: PI, 23: CE, 24: RN
 # 25: PB, 26: PE, 27: AL, 28: SE, 29: BA, 31: MG, 32: ES, 33: RJ, 35: SP, 41: PR, 42: SC, 43: RS
 # 50: MS, 51: MT, 52: GO, 53: DF 
+
+dados_sim_2 = dados_sim_1[substr(dados_sim_1$CODMUNRES, 1, 2) == "52",]
 
 # observar abaixo o número de óbitos por UF de residência para certificar-se que seu banco de dados está correto
 # 11: 7948      12: 3517      13: 16675     14: 2091      15: 37365     16: 2946       17: 7402
@@ -500,6 +507,8 @@ write.csv(SINASC_GO, "SINASC_GO.csv", row.names = FALSE)
 # 50: 15457     51: 17095     52: 38854     53: 11975
 
 # Exportar o arquivo com o nome dados_sim_2.csv
+
+write.csv(dados_sim_2, "dados_sim_2.csv", row.names = FALSE)
 
 # Ao concluir a Tarefa 3 da Etapa 2 commite e envie para o repositório REMOTO o script e dados_sim_2.csv com o comentário "Dados do estado UF (coloque o nome da UF) e script de sua obtenção"
 
